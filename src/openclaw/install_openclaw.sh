91215dd3d98a98f70f4a9cfb69ebec6fd1f792e2
sullyschoice <sullyschoice>
2026-03-01 09:30:18 -0500

    Remove copied desktop file. Add action schedule

--- src/openclaw/install_openclaw.sh
+++ src/openclaw/install_openclaw.sh
@@ -17,22 +17,3 @@ rm install.sh
 chown -R 1000:1000 /opt/openclaw
 
 curl -fsSL https://openclaw.ai/install.sh | bash -s -- --install-method git
-
-cat >$HOME/Desktop/openclaw.desktop <<EOL
-[Desktop Entry]
-Version=1.0
-Type=Application
-Name=Unreal Tournament 2004
-GenericName=Game
-Comment=Unreal Tournament 2004
-Exec=/opt/ut2004/launch.sh %F
-Path=/opt/ut2004/
-Terminal=false
-MimeType=text/plain;
-Icon=/opt/ut2004/Help/Unreal.ico
-Categories=Graphics;Utility;
-StartupNotify=true
-EOL
-
-chmod +x $HOME/Desktop/openclaw.desktop
-chown 1000:1000 $HOME/Desktop/openclaw.desktop
